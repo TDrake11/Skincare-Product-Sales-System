1a83ae6bfb89298bedf79a2926e1103c0fb50a85
TDrake11 <tantqse172039@fpt.edu.vn>
2025-02-11 12:51:20 +0700

    Update Connection String, ApplicationDbContext and AutoMirgation

--- Skincare Product Sales System Infrastructure/Data/ApplicationDbContext.cs
+++ Skincare Product Sales System Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Skincare_Product_Sales_System_Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace Skincare_Product_Sales_System_Infrastructure.Data
 {
-	internal class ApplicationDbContext
+	public class ApplicationDbContext : IdentityDbContext<User>
 	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options) { }
+
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Comment> Comments { get; set; }
+		public DbSet<Order>	Orders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+		public DbSet<SkinTest> SkinTests { get; set; }
+		public DbSet<SkinAnswer> SkinAnswers { get; set; }
+		public DbSet<SkinQuestion> SkinQuestions { get; set; }
+		public DbSet<SkinTestAnswer> SkinTestAnswers { get; set; }
+		public DbSet<SkinType> SkinTypes { get; set; }
+		public DbSet<SkinCareRoutine> SkinCareRoutines { get; set; }
+		public DbSet<StepRoutine> StepRoutines { get; set; }
+
 	}
 }
--- Skincare Product Sales System/Program.cs
+++ Skincare Product Sales System/Program.cs
@@ -1,15 +1,23 @@
 
+using Skincare_Product_Sales_System_Infrastructure.Extensions;
+
 namespace Skincare_Product_Sales_System
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+			var services = builder.Services;
+			var config = builder.Configuration;
 
 			// Add services to the container.
+			services.AddInfrastructure(config);
+
+
 
 			builder.Services.AddControllers();
+
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
@@ -29,6 +37,8 @@ namespace Skincare_Product_Sales_System
 
 
 			app.MapControllers();
+
+			await app.AddAutoMigrateDatabase();
 
 			app.Run();
 		}
