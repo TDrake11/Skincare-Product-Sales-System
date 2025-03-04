10a53083ce9fa758cdba3c97a1b605dbcc0fd206
TDrake11 <tantqse172039@fpt.edu.vn>
2025-03-04 10:03:51 +0700

    Account Controller: Get User Profile

---
+++ Skincare Product Sales System/Models/UserProfileModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Skincare_Product_Sales_System.Models
+{
+	public class UserProfileModel
+	{
+		public  string? FirstName { get; set; }
+		public  string? LastName { get; set; }
+		public  string? Address { get; set; }
+		public  DateOnly? Birthday { get; set; }
+		[EmailAddress]
+		public required string Email { get; set; }
+		public string? PhoneNumber { get; set; }
+		public string? Avatar { get; set; }
+
+	}
+}
