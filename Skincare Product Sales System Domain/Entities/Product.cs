a5f7d383d10e5d86f4c86a5b701906792b1bb1b3
TDrake11 <tantqse172039@fpt.edu.vn>
2025-03-08 21:41:07 +0700

    Fix CreateProductV2

--- Skincare Product Sales System Domain/Entities/Product.cs
+++ Skincare Product Sales System Domain/Entities/Product.cs
@@ -12,8 +12,8 @@ namespace Skincare_Product_Sales_System_Domain.Entities
 	{
 		public string ProductName { get; set; }
 		public string Description { get; set; }
-		public DateOnly CreatedDate { get; set; }
-		public DateOnly ExpiredDate { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime ExpiredDate { get; set; }
 		public double Price { get; set; }
 		public int Quantity { get; set; }
 		public string? Image { get; set; }
--- Skincare Product Sales System Domain/Entities/SkinType.cs
+++ Skincare Product Sales System Domain/Entities/SkinType.cs
@@ -9,7 +9,7 @@ namespace Skincare_Product_Sales_System_Domain.Entities
 	public class SkinType : BaseEntity
 	{
 		public string SkinTypeName { get; set; }
-		public string SkinTypeStatus { get; set; }
+		public string? SkinTypeStatus { get; set; }
 		public virtual ICollection<Product>? Products { get; set; } // 1 skin type co nhieu product
 		public virtual ICollection<SkinCareRoutine>? SkinCareRoutines { get; set; } // 1 skin type co nhieu routine
 		public virtual ICollection<SkinTest>? SkinTests { get; set; } // 1 skin type co nhieu skin test
