8cc3f20967361e0bda65b7f1a2692ecbc0677901
HuynhHoaa <hoahuynh882003@gmail.com>
2025-03-11 19:15:11 +0700

    CRUD StepRoutine_V2

---
+++ Skincare Product Sales System Application/Services/SkinAnswerService/ISkinAnswerService.cs
@@ -0,0 +1,20 @@
+﻿using Skincare_Product_Sales_System_Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skincare_Product_Sales_System_Application.Services.SkinAnswerService
+{
+    public interface ISkinAnswerService
+    {
+        Task<List<SkinAnswer>> GetAllSkinAnswer();
+        Task<SkinAnswer> GetSkinAnswerById(int id);
+        Task<List<SkinAnswer>> GetSkinAnswersBySkinTypeId(int skinTypeId);
+        Task<List<SkinAnswer>> GetSkinAnswersBySkinQuestionId(int skinQuestionId);
+        Task AddSkinAnswer(SkinAnswer skinAnswer);
+        Task UpdateSkinAnswer(SkinAnswer skinAnswer);
+        Task DeleteSkinAnswer(int id);
+    }
+}
