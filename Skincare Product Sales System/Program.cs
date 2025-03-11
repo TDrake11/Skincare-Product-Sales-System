837b0cbea178527e8177862e25cb36ba5eb3661c
HuynhHoaa <hoahuynh882003@gmail.com>
2025-03-11 19:13:44 +0700

    CRUD StepRoutine, chỉnh sửa lại tên class của SkinAnswer cho đúng

--- Skincare Product Sales System/Program.cs
+++ Skincare Product Sales System/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Skincare_Product_Sales_System.Extensions;
+using Skincare_Product_Sales_System_Application.Service.StepRoutineService;
 using Skincare_Product_Sales_System_Application.Services.CategoryService;
 using Skincare_Product_Sales_System_Application.Services.CommentService;
 using Skincare_Product_Sales_System_Application.Services.OrderDetailService;
@@ -10,6 +11,7 @@ using Skincare_Product_Sales_System_Application.Services.ProductService;
 using Skincare_Product_Sales_System_Application.Services.SkinAnswerService;
 using Skincare_Product_Sales_System_Application.Services.SkinQuestionService;
 using Skincare_Product_Sales_System_Application.Services.SkinTypeService;
+using Skincare_Product_Sales_System_Application.Services.StepRoutineServices;
 using Skincare_Product_Sales_System_Domain.Entities;
 using Skincare_Product_Sales_System_Infrastructure.Data;
 using Skincare_Product_Sales_System_Infrastructure.Extensions;
@@ -37,6 +39,16 @@ namespace Skincare_Product_Sales_System
 
 			services.AddPresentation(config);
 			services.AddIdentityService(config);
+
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IProductService, ProductService>();
+			builder.Services.AddScoped<ICommentService, CommentService>();
+			builder.Services.AddScoped<IOrderService, OrderService>();
+			builder.Services.AddScoped<IOrderDetailService, OrderDetailService>();
+			builder.Services.AddScoped<ISkinQuestionService, SkinQuestionService>();
+			builder.Services.AddScoped<ISkinAnswerService,  SkinAnswerService>();
+			builder.Services.AddScoped<ISkinTypeService, SkinTypeService>();
+			builder.Services.AddScoped<IStepRoutineService, StepRoutineService>();
 
 
 			var app = builder.Build();
