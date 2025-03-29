16aedf30bdd5012c98bc42210e55f0ba76ad20d2
TDrake11 <141726163+TDrake11@users.noreply.github.com>
2025-03-29 18:07:36 +0700

    Xem danh sách và tạo mới của Entity SkinTest (#32)
    * CRUD SkinCareRoutine
    
    * CRUD SkinCareRoutine_V2 (sửa lại chức năng create và update)
    
    * StepRoutine sửa Get danh sách theo RoutineId
    
    * Fix TotalSteps người dùng không được quyền nhập giá trị vào nữa mà nó sẽ tự động được tính qua Add và Delete StepRoutine (Khi AddStepRoutine sẽ tự động cộng thêm1 giá trị cho TotalSteps và tương tự với Delete thì từ 1 giá trị)
    
    * CRUD Comment thêm LastName và Avatar của Customer khi lấy danh sách
    
    * CRUD Comment thêm FirstName hiển thị cho người dùng xem
    
    * Chuyển đổi thông báo khi không tìm thấy Id 404 thành 200 kèm thông báo
    
    * Sửa lại thông báo cho chức năng Delete cho các Entity và cập nhật lại chức năng Update cho SkinAnswer và SkinQuestion hiện Status
    
    * Câp nhật chức năng Update của SkinQuestion với SkinAnswer chỉ nhận 2 giá trị là Active với Inactive
    
    * Xem danh sách và tạo mới của Entity SkinTest
    
    ---------
    
    Co-authored-by: HuynhHoaa <hoahuynh882003@gmail.com>

---
+++ Skincare Product Sales System/Models/SkinTestModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Skincare_Product_Sales_System.Models
+{
+    public class SkinTestModel
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string SkinTestStatus { get; set; }
+        public string CustomerId {  get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int SkinTypeId { get; set; }
+        public string SkinTypeName { get; set; }
+    }
+
+    public class CreateSkinTestModel
+    {
+        public string CustomerId { get; set; }
+        public int SkinTypeId { get; set; }
+    }
+}
