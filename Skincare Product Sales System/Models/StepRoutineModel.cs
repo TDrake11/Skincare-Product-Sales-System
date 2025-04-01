67c9ee511d2b051e6f4a4ca3324f6f37d35fce74
TDrake11 <tantqse172039@fpt.edu.vn>
2025-04-01 13:21:38 +0700

    Fix run

--- Skincare Product Sales System/Models/StepRoutineModel.cs
+++ Skincare Product Sales System/Models/StepRoutineModel.cs
@@ -24,7 +24,6 @@
     {
         public int Id { get; set; }
         public int StepNumber { get; set; }
-        public string StepDescription { get; set; }
         public string? StepDescription { get; set; }
         public string? Status { get; set; }
         public int RoutineId { get; set; }
